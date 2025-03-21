8b58f8064d031413bd1292a42cd00130660d7107
GuiBarao <guibarao04@gmail.com>
2025-03-21 17:24:16 -0300

    Criando componente Acordeao

---
+++ src/components/Acordeao.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Button from '@mui/material/Button';
+
+function Acordeao() {
+    return (
+        <Accordion defaultExpanded>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3-content"
+            id="panel3-header"
+          >
+            <Typography component="span">Accordion Actions</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </AccordionDetails>
+          <AccordionActions>
+            <Button>Cancel</Button>
+            <Button>Agree</Button>
+          </AccordionActions>
+        </Accordion>
+
+    );
+}
+
+export default Acordeao;
