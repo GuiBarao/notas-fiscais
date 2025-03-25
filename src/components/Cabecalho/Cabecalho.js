ab57250aebeb35142a11c9284e78f7e03d621433
GuiBarao <guibarao04@gmail.com>
2025-03-25 11:12:06 -0300

    Adicionando header

--- src/components/Cabecalho/Cabecalho.js
+++ src/components/Cabecalho/Cabecalho.js
@@ -1,7 +1,12 @@
+import styles from "./Cabecalho.module.css"
+
 function Cabecalho ()
 {
     return (
-        "r456"
+        <header className={styles.cabecalho}>
+            <h1>Notas Fiscais</h1>
+
+        </header>
     );
 }
 
--- src/pages/Notas/index.js
+++ src/pages/Notas/index.js
@@ -2,15 +2,19 @@ import AcordeaoFilial from "../../components/AcordeaoFilial/AcordeaoFilial";
 import filiais from "../../json/db.json"
 import CampoFiltro from "../../components/CampoFiltro/CampoFiltro.js"
 import styles from "./Notas.module.css";
+import Cabecalho from "../../components/Cabecalho/Cabecalho.js";
+
 
 function Notas () {
     return (
         <section className={styles.notas}>
+            <CampoFiltro />
+            
+            <div className={styles.header_filtros_wrapper}>
+                
+                <Cabecalho />
 
-            <div className={styles.filtro_notas}>
-                <CampoFiltro />
-
-                <div className={styles.acordeoes_filiais}>
+                <div>
                     {filiais.map( (filial) => {
                         return <AcordeaoFilial {...filial} />
                     })}
