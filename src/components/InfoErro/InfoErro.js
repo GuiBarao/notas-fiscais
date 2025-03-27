7588f2019951d65c11b0fff18dd75e981dc8cfb2
GuiBarao <guibarao04@gmail.com>
2025-03-27 14:58:43 -0300

    Estilizando InfoErro

--- src/components/InfoErro/InfoErro.js
+++ src/components/InfoErro/InfoErro.js
@@ -2,18 +2,18 @@ import styles from "./InfoErro.module.css";
 
 function InfoErro({mensagem, log}) {
     return  (
-        <section className={styles.InfoErro}>
+        <section className={styles.infoErro}>
 
-            <div className={styles.wrapper_mensagem}>
-                <h1>Mensagem:</h1>
-                <p>{mensagem}</p>
+            <div className={styles.wrapper_titulo_info}>
+                <h1 className={styles.titulo}>Mensagem:</h1>
+                <p className={styles.info}>{mensagem}</p>
             </div>
 
             <div className={styles.linha}></div>
 
-            <div className={styles.wrapper_log}>
-                <h1>Log:</h1>
-                <p>{log}</p>
+            <div className={styles.wrapper_titulo_info}>
+                <h1 className={styles.titulo}>Log:</h1>
+                <p className={styles.info}>{log}</p>
             </div>
 
         </section>
