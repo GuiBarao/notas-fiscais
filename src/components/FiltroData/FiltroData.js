16f98c6702bc3d0ac3cb40b529d1194e87b7cc88
GuiBarao <guibarao04@gmail.com>
2025-04-09 16:35:54 -0300

    Ajustando locale da data

--- src/components/FiltroData/FiltroData.js
+++ src/components/FiltroData/FiltroData.js
@@ -2,12 +2,19 @@ import * as React from 'react';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-
+import { ptBR } from '@mui/x-date-pickers/locales';
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br';
 
 import styles from "./FiltroData.module.css"
 
 function FiltroData({inicioValue, inicioOnChange, fimValue, fimOnChange}) {
 
+
+    React.useEffect(() => {
+        dayjs.locale('pt-br');
+    }, []);
+    
 
     const props = {
     textField: {
@@ -16,8 +23,11 @@ function FiltroData({inicioValue, inicioOnChange, fimValue, fimOnChange}) {
     },
     };
 
+
     return (
-            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="br">
+            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt-br"
+            localeText={ptBR.components.MuiLocalizationProvider.defaultProps.localeText}>
+                
                 <div className={styles.date_picker_wrapper}>
                     
                     <DatePicker className={styles.date_picker}  format="DD/MM/YYYY" 
