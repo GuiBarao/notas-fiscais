4268418580c2d07afccda5b7913533c71e7e3f98
GuiBarao <guibarao04@gmail.com>
2025-07-19 08:52:21 -0400

    Carregando notas

--- src/pages/notas/AcordeaoFilial/index.js
+++ src/pages/notas/AcordeaoFilial/index.js
@@ -14,6 +14,8 @@ import FunctionsIcon from '@mui/icons-material/Functions';
 import EditNoteIcon from '@mui/icons-material/EditNote';
 import { HttpStatusCode } from 'axios';
 import { useSnackbar } from 'notistack';
+import LinearProgress from '@mui/material/LinearProgress';
+
 
 
 function AcordeaoFilial({ filial, valor_teto, filtroDataInicio, filtroDataFim, 
@@ -22,15 +24,19 @@ function AcordeaoFilial({ filial, valor_teto, filtroDataInicio, filtroDataFim,
 
 
     const [notas, setNotas] = useState([]);
+    const [carregandoNotas, setCarregandoNotas] = useState(false)
     const { enqueueSnackbar } = useSnackbar();
 
     const notas_filial = async (filial) => {
         try {
+            setCarregandoNotas(true)
             const response = await buscarNotas(filial, filtroDataInicio, filtroDataFim);
+            setCarregandoNotas(false)
             setNotas(response);
             
         }   
         catch(error) {
+            setCarregandoNotas(false)
             if(error.status === HttpStatusCode.Unauthorized) {
               enqueueSnackbar("Ação não autorizada", { variant: "error" });
 
@@ -72,13 +78,6 @@ function AcordeaoFilial({ filial, valor_teto, filtroDataInicio, filtroDataFim,
       sx_accordion.opacity = "0.6";
     }
 
-    const sx_summary = {
-        display : "flex",
-        flexDirection: "row",
-        width: "100%",
-        height: "40px",
-        margin: "0px",
-    }
 
     const sx_typography = {
       display : "flex",
@@ -94,48 +93,55 @@ function AcordeaoFilial({ filial, valor_teto, filtroDataInicio, filtroDataFim,
         expanded = {!desabilitaAcordeao && expanded}
         onChange={() => setExpanded(!expanded)}
         >
-          
+
           <AccordionSummary
             expandIcon={<ExpandMoreIcon sx={{color: "icon.secondary"}} />}
             aria-controls="panel3-content"
             id="panel3-header"
-            sx={sx_summary}
-          >
-
-
-            <Typography sx = {sx_typography}>
-                <PlaceIcon />
-                {filial} 
-            </Typography>
-
-            <Typography  sx = {sx_typography}>
-                <CurrencyExchangeIcon />
-                {`Valor Teto: R$${valor_teto.toFixed(2)}`}
-            </Typography>
-
-            <Typography  sx = {sx_typography}>
-              {desabilitaAcordeao ? 
-                <span className="text-red-800 opacity-100">* Nenhuma nota foi encontrada.</span> :
-
-                <Box sx={sx_typography}>
-                  <FunctionsIcon />
-                  {`Valor Total: R$${somatorioValores.toFixed(2)}`}
-                </Box>}
-                  
-            </Typography>
+            sx={{ width: "100%",
+                  height: "40px",
+                  margin: "0px"}}>
             
-            
-           
-            <EditNoteIcon sx={{color: "icon.secondary"}} 
-              onClick={(e) => {
-                  e.stopPropagation();
-                  
-                  setEdicaoValorTeto({ativado : true, nome_filial: filial});
-                }}/>
+              {carregandoNotas ? <Box sx={{ width: "100%", height: "5px", alignSelf: "center" }}> <LinearProgress color='success' /> </Box> : 
 
-            
-            
+              <Box sx={{display : "flex",
+                        flexDirection: "row",
+                        width: "100%"}}>
+
+                <Typography sx = {sx_typography}>
+                    <PlaceIcon />
+                    {filial} 
+                </Typography>
+
+                <Typography  sx = {sx_typography}>
+                    <CurrencyExchangeIcon />
+                    {`Valor Teto: R$${valor_teto.toFixed(2)}`}
+                </Typography>
+
+                <Typography  sx = {sx_typography}>
+                  {desabilitaAcordeao ? 
+                    <span className="text-red-800 opacity-100">* Nenhuma nota foi encontrada.</span> :
+
+                    <Box sx={sx_typography}>
+                      <FunctionsIcon />
+                      {`Valor Total: R$${somatorioValores.toFixed(2)}`}
+                    </Box>}
+                      
+                </Typography>
+                
+                
+              
+                <EditNoteIcon sx={{color: "icon.secondary"}} 
+                  onClick={(e) => {
+                      e.stopPropagation();
+                      
+                      setEdicaoValorTeto({ativado : true, nome_filial: filial});
+                    }}/>
+              </Box>
+            }   
           </AccordionSummary>
+
+
           <AccordionDetails className="flex flex-col ">
               
             <TabelaNotas notas = {notas_filtradas}/>
